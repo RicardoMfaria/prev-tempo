cc60893dd66d74ae693722b7e5cffb259684cf28
Ricardo Marques <contato.ricardotr@gmail.com>
2025-06-14 08:11:59 -0300

    ajuste

--- src/components/utils/weatherClient.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-import env from 'react-dotenv'
-export default axios.create({
-    baseURL:'http://localhost:3000/', 
-    headers: {
-        Authorization: process.env.WEATHER_KEY
-    }
-})
